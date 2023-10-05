fbc9abf13e948f80160282f0d904de825e134bd1
Nika <user@Nikas-MacBook-Pro.local>
2023-10-06 00:51:07 +0400

    checkbox component
    radio button
    
    fixed
    
    radio outline color
    
    styling fixes - theme

---
+++ src/assets/SVGs/Unchecked.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Svg, { SvgProps, Rect } from 'react-native-svg';
+
+export const Unchecked = (props: SvgProps) => (
+  <Svg width={18} height={18} fill="none" {...props}>
+    <Rect width={17} height={17} x={0.5} y={0.5} stroke="#1D1D1D" strokeOpacity={0.2} rx={3.5} />
+  </Svg>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { Toast } from './Toast/Toast';
 export { Text } from './Text/Text';
 export { TextInput } from './TextInput/TextInput';
+export { Checkbox } from './Checkbox';
+export { Radio } from './Radio';
