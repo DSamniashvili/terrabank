5b2354436e1e851cbddad81039c7c24fce2758e2
Anakin <anakin@Anakins-MacBook-Pro.local>
2023-11-10 14:49:27 +0400

    skeleton fix

--- src/components/Skeleton/DashboardSkeleton.tsx
+++ src/components/Skeleton/DashboardSkeleton.tsx
@@ -11,42 +11,39 @@ export const DashboardSkeleton = () => {
         borderRadius={8}
         paddingVertical={35}
         width={'100%'}
-        backgroundColor="rgba(254, 254, 254, 1)"
         marginHorizontal={14}
       >
-        <SkeletonPlaceholder>
-          <SkeletonPlaceholder.Item flexDirection="row" justifyContent="space-between">
-            <SkeletonPlaceholder.Item>
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(211)}
-                height={verticalScale(20)}
-                borderRadius={8}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(211)}
-                height={verticalScale(20)}
-                borderRadius={8}
-                marginTop={20}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(121)}
-                height={verticalScale(38)}
-                borderRadius={54}
-                marginTop={16}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-            </SkeletonPlaceholder.Item>
+        <SkeletonPlaceholder.Item flexDirection="row" justifyContent="space-between">
+          <SkeletonPlaceholder.Item>
             <SkeletonPlaceholder.Item
-              width={140}
-              height={127}
+              width={horizontalScale(211)}
+              height={verticalScale(20)}
               borderRadius={8}
               backgroundColor="rgba(225, 230, 239, 1)"
             />
+
+            <SkeletonPlaceholder.Item
+              width={horizontalScale(211)}
+              height={verticalScale(20)}
+              borderRadius={8}
+              marginTop={20}
+              backgroundColor="rgba(225, 230, 239, 1)"
+            />
+            <SkeletonPlaceholder.Item
+              width={horizontalScale(121)}
+              height={verticalScale(38)}
+              borderRadius={54}
+              marginTop={16}
+              backgroundColor="rgba(225, 230, 239, 1)"
+            />
           </SkeletonPlaceholder.Item>
-        </SkeletonPlaceholder>
+          <SkeletonPlaceholder.Item
+            width={140}
+            height={127}
+            borderRadius={8}
+            backgroundColor="rgba(225, 230, 239, 1)"
+          />
+        </SkeletonPlaceholder.Item>
       </SkeletonPlaceholder.Item>
       <SkeletonPlaceholder.Item
         borderRadius={8}
@@ -56,43 +53,40 @@ export const DashboardSkeleton = () => {
         marginHorizontal={14}
         backgroundColor="rgba(254, 254, 254, 1)"
       >
-        <SkeletonPlaceholder>
-          <SkeletonPlaceholder.Item backgroundColor="rgba(254, 254, 254, 1)">
-            <SkeletonPlaceholder.Item>
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(144)}
-                height={verticalScale(20)}
-                borderRadius={8}
-                marginBottom={8}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-            </SkeletonPlaceholder.Item>
+        <SkeletonPlaceholder.Item backgroundColor="rgba(254, 254, 254, 1)">
+          <SkeletonPlaceholder.Item>
+            <SkeletonPlaceholder.Item
+              width={horizontalScale(144)}
+              height={verticalScale(20)}
+              borderRadius={8}
+              marginBottom={8}
+              backgroundColor="rgba(225, 230, 239, 1)"
+            />
           </SkeletonPlaceholder.Item>
-        </SkeletonPlaceholder>
+        </SkeletonPlaceholder.Item>
+
         <SkeletonPlaceholder.Item marginLeft={5}>
-          <SkeletonPlaceholder>
-            <View style={{ flexDirection: 'row' }}>
-              {[...Array(4)].map((_, index) => (
-                <View key={index}>
-                  <SkeletonPlaceholder.Item
-                    width={50}
-                    height={50}
-                    marginTop={7}
-                    marginRight={45}
-                    borderRadius={50}
-                    backgroundColor="rgba(225, 230, 239, 1)"
-                  />
-                  <SkeletonPlaceholder.Item
-                    width={50}
-                    height={10}
-                    borderRadius={50}
-                    marginTop={5}
-                    backgroundColor="rgba(225, 230, 239, 1)"
-                  />
-                </View>
-              ))}
-            </View>
-          </SkeletonPlaceholder>
+          <View style={{ flexDirection: 'row' }}>
+            {[...Array(4)].map((_, index) => (
+              <View key={index}>
+                <SkeletonPlaceholder.Item
+                  width={50}
+                  height={50}
+                  marginTop={7}
+                  marginRight={45}
+                  borderRadius={50}
+                  backgroundColor="rgba(225, 230, 239, 1)"
+                />
+                <SkeletonPlaceholder.Item
+                  width={50}
+                  height={10}
+                  borderRadius={50}
+                  marginTop={5}
+                  backgroundColor="rgba(225, 230, 239, 1)"
+                />
+              </View>
+            ))}
+          </View>
         </SkeletonPlaceholder.Item>
       </SkeletonPlaceholder.Item>
       <Divider style={{ marginTop: 20 }} />
@@ -104,55 +98,52 @@ export const DashboardSkeleton = () => {
         width="100%"
         marginHorizontal={14}
       >
-        <SkeletonPlaceholder>
-          <SkeletonPlaceholder.Item
-            backgroundColor="rgba(34, 39, 47, 1)"
-            flexDirection="row"
-            alignItems="center"
-            width="100%"
-            justifyContent="space-between"
-          >
-            <SkeletonPlaceholder.Item>
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(247)}
-                height={verticalScale(28)}
-                borderRadius={16}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-            </SkeletonPlaceholder.Item>
+        <SkeletonPlaceholder.Item
+          backgroundColor="rgba(254, 254, 254, 1)"
+          flexDirection="row"
+          alignItems="center"
+          width="100%"
+          justifyContent="space-between"
+        >
+          <SkeletonPlaceholder.Item>
+            <SkeletonPlaceholder.Item
+              width={horizontalScale(247)}
+              height={verticalScale(28)}
+              borderRadius={16}
+              backgroundColor="rgba(225, 230, 239, 1)"
+            />
           </SkeletonPlaceholder.Item>
-        </SkeletonPlaceholder>
-        <SkeletonPlaceholder>
-          <SkeletonPlaceholder.Item flexDirection="row" marginTop={10}>
-            <SkeletonPlaceholder.Item>
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(163)}
-                height={verticalScale(188)}
-                borderRadius={16}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-            </SkeletonPlaceholder.Item>
+        </SkeletonPlaceholder.Item>
 
-            <SkeletonPlaceholder.Item>
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(163)}
-                height={verticalScale(188)}
-                borderRadius={16}
-                marginLeft={10}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-            </SkeletonPlaceholder.Item>
-            <SkeletonPlaceholder.Item>
-              <SkeletonPlaceholder.Item
-                width={horizontalScale(163)}
-                height={verticalScale(188)}
-                borderRadius={16}
-                marginLeft={10}
-                backgroundColor="rgba(225, 230, 239, 1)"
-              />
-            </SkeletonPlaceholder.Item>
+        <SkeletonPlaceholder.Item flexDirection="row" marginTop={10}>
+          <SkeletonPlaceholder.Item>
+            <SkeletonPlaceholder.Item
+              width={horizontalScale(163)}
+              height={verticalScale(188)}
+              borderRadius={16}
+              backgroundColor="rgba(225, 230, 239, 1)"
+            />
           </SkeletonPlaceholder.Item>
-        </SkeletonPlaceholder>
+
+          <SkeletonPlaceholder.Item>
+            <SkeletonPlaceholder.Item
+              width={horizontalScale(163)}
+              height={verticalScale(188)}
+              borderRadius={16}
+              marginLeft={10}
+              backgroundColor="rgba(225, 230, 239, 1)"
+            />
+          </SkeletonPlaceholder.Item>
+          <SkeletonPlaceholder.Item>
+            <SkeletonPlaceholder.Item
+              width={horizontalScale(163)}
+              height={verticalScale(188)}
+              borderRadius={16}
+              marginLeft={10}
+              backgroundColor="rgba(225, 230, 239, 1)"
+            />
+          </SkeletonPlaceholder.Item>
+        </SkeletonPlaceholder.Item>
       </SkeletonPlaceholder.Item>
     </>
   );
